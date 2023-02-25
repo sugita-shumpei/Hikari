13edd299e854eda3070e5e3baa3fb41eb2629f44
shumpei.sugita <sugita.shumpei@fj.ics.keio.ac.jp>
2023-02-26 01:02:52 +0900

    Fix

--- Test/Test0/Include/Test0.h
+++ Test/Test0/Include/Test0.h
@@ -4,4 +4,22 @@
 #include <Hikari/Math/Mat.h>
 #include <cmath>
 #include <cassert>
+#include <iostream>
+
+template<typename T>
+void ShowMat3(const Hikari::TMat3<T>& v)
+{
+	std::cout << v.Col0_0() << "," << v.Col0_1() << "," << v.Col0_2() << std::endl;
+	std::cout << v.Col1_0() << "," << v.Col1_1() << "," << v.Col1_2() << std::endl;
+	std::cout << v.Col2_0() << "," << v.Col2_1() << "," << v.Col2_2() << std::endl;
+}
+
+template<typename T>
+void ShowMat4(const Hikari::TMat4<T>& v)
+{
+	std::cout << v.Col0_0() << "," << v.Col0_1() << "," << v.Col0_2() << "," << v.Col0_3() << std::endl;
+	std::cout << v.Col1_0() << "," << v.Col1_1() << "," << v.Col1_2() << "," << v.Col1_3() << std::endl;
+	std::cout << v.Col2_0() << "," << v.Col2_1() << "," << v.Col2_2() << "," << v.Col2_3() << std::endl;
+	std::cout << v.Col3_0() << "," << v.Col3_1() << "," << v.Col3_2() << "," << v.Col3_3() << std::endl;
+}
 #endif
--- Test/Test0/Src/Test0.cpp
+++ Test/Test0/Src/Test0.cpp
@@ -6,5 +6,36 @@ int main(int argc, const char* argv[])
 	constexpr auto v2 = AnyOf(TVec4<int>(4, 4, 4).GreaterThan(TVec4<int>(4, 4, 3)));
 	constexpr auto v3 = AddOf(TVec4<int>(4, 4, 4).GreaterThan(TVec4<int>(4, 4, 3)));
 	assert(Normalize(TVec3<float>(4))== Normalize(TVec3<float>::Ones()));
+	
+	std::cout << TMat3<float>(
+		11, 2, 1,
+		2,  3, 1,
+		31, 0, 2
+	).Det() << std::endl;
+
+	std::cout << TMat4<float>(
+		11, 2, 1,3,
+		 2, 3, 1,4,
+		31, 0, 2,5,
+		 6, 7, 8,9
+		).Det() << std::endl;
+
+	ShowMat4(TMat4<float>(
+		11, 2, 1, 3,
+		2, 3, 1, 4,
+		31, 0, 2, 5,
+		6, 7, 8, 9
+	).Inverse());
+
+	ShowMat4(TMat4<float>(
+		11, 2, 1, 3,
+		2, 3, 1, 4,
+		31, 0, 2, 5,
+		6, 7, 8, 9
+		).Inverse()* TMat4<float>(
+			11, 2, 1, 3,
+			2, 3, 1, 4,
+			31, 0, 2, 5,
+			6, 7, 8, 9));
 	return 0;
 }
